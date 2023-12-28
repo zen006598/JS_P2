6be5bb322a52113f03c2c1b97c669965d2edefdb
7936 陳榮葦 <7936@colatour.com.tw>
2023-12-28 16:46:32 +0800

    add data

---
+++ model.js
@@ -0,0 +1,26 @@
+const itemList = [
+  {
+    id: 397458,
+    title: "product001",
+    price: 100,
+    quantity: 10,
+    img: "https://picsum.photos/200",
+  },
+
+  {
+    id: 944774,
+    title: "product002",
+    price: 200,
+    quantity: 5,
+    img: "https://picsum.photos/201",
+  },
+
+  {
+    id: 767771,
+    title: "product003",
+    price: 300,
+    quantity: 15,
+    img: "https://picsum.photos/301",
+  },
+];
+export { itemList };
